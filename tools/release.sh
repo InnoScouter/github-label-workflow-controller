82cef064cb830a049758475ce9526e26d08a5654
t.momose <hyakkun@gmail.com>
2024-09-30 18:13:50 +0900

    Fix indent

--- tools/release.sh
+++ tools/release.sh
@@ -2,11 +2,11 @@ rm -rf artifact && mkdir artifact
 (cd dist && zip -r ../artifact/function.zip .)
 
 aws-vault exec is-dev -- aws \
- s3api put-object \
+  s3api put-object \
   --bucket dev-innoscouter-lambda-function \
   --key github-label-workflow-controller/function.zip \
   --body artifact/function.zip
 
 aws-vault exec is-dev -- aws \
-lambda update-function-code --function-name github-label-workflow-controller \
---s3-bucket dev-innoscouter-lambda-function --s3-key github-label-workflow-controller/function.zip
+  lambda update-function-code --function-name github-label-workflow-controller \
+  --s3-bucket dev-innoscouter-lambda-function --s3-key github-label-workflow-controller/function.zip
